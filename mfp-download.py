6dbfea8b3abbeb3666f1194dee7d4240492a5a5b
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2022-07-28 14:21:07 -0400

    Start script to download from MFP

---
+++ mfp-download.py
@@ -0,0 +1,26 @@
+import myfitnesspal as mfp
+import csv, sys, os
+from datetime import datetime
+import pandas as pd
+
+client = mfp.Client("calrkarnold")
+
+start_date = datetime(2015, 1, 1)
+today = datetime.today().date()
+date_range = pd.date_range(start_date, today)
+
+day_num = 1
+food_diary = []
+
+for day in pd.date_range(datetime(2018, 7, 20), datetime(2018, 7, 30)):
+    mfp_day = client.get_date(day)
+
+    food_diary.append(
+        {
+            "Date": day,
+            "Totals": mfp_day.totals
+        }
+    )
+
+df = pd.DataFrame.from_dict(food_diary)
+df = pd.concat([df, df["Totals"].apply(pd.Series)], axis=1)
